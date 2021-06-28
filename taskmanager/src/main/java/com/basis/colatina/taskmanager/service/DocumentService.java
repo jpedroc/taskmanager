dc52c9c45c0839de59d0b8123725ef4f1aa410fa
joao.calixto <joao.calixto@basis.com.br>
2021-06-28 20:32:04 -0300

    SERVIÇOS - serviços e dtos criados

--- taskmanager/src/main/java/com/basis/colatina/taskmanager/service/DocumentService.java
+++ taskmanager/src/main/java/com/basis/colatina/taskmanager/service/DocumentService.java
@@ -1,4 +1,22 @@
 package com.basis.colatina.taskmanager.service;
 
+import com.basis.colatina.taskmanager.service.dto.DocumentDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class DocumentService {
+
+    public DocumentDTO save(DocumentDTO documentDTO) {
+        return null;
+    }
+
+    public DocumentDTO findOne(DocumentDTO documentDTO) {
+        return null;
+    }
+
+    public void Delete(Integer id){}
 }
