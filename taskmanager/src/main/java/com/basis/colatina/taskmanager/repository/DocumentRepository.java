f9f97935b1d367c5c27311d29ed3701de83ab34b
joao.calixto <joao.calixto@basis.com.br>
2021-06-28 21:39:40 -0300

    SERVIÇO - document service finalizado

--- taskmanager/src/main/java/com/basis/colatina/taskmanager/repository/DocumentRepository.java
+++ taskmanager/src/main/java/com/basis/colatina/taskmanager/repository/DocumentRepository.java
@@ -4,6 +4,9 @@ import com.basis.colatina.taskmanager.domain.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
+    List<Document> findAllByTaskId(Integer id);
 }
