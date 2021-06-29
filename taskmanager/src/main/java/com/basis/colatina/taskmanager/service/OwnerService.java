ed2b8ca47dc513ed72f5d16f37a7638c1a5b1a53
joao.calixto <joao.calixto@basis.com.br>
2021-06-28 21:33:40 -0300

    SERVIÇO - owner service finalizado

--- taskmanager/src/main/java/com/basis/colatina/taskmanager/service/OwnerService.java
+++ taskmanager/src/main/java/com/basis/colatina/taskmanager/service/OwnerService.java
@@ -1,23 +1,45 @@
 package com.basis.colatina.taskmanager.service;
 
+import com.basis.colatina.taskmanager.domain.Owner;
+import com.basis.colatina.taskmanager.repository.OwnerRepository;
 import com.basis.colatina.taskmanager.service.dto.OwnerDTO;
+import com.basis.colatina.taskmanager.service.exception.BadRequestAlertException;
+import com.basis.colatina.taskmanager.service.mapper.OwnerMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class OwnerService {
 
+    private final OwnerRepository ownerRepository;
+    private final OwnerMapper ownerMapper;
+
     public OwnerDTO save(OwnerDTO ownerDTO) {
-        return null;
+        if (ownerDTO.getBirthDate().isAfter(LocalDate.now())) {
+            throw new BadRequestAlertException("Invalid Birth Date");
+        }
+
+        Owner owner = ownerRepository.save(ownerMapper.toEntity(ownerDTO));
+
+        return ownerMapper.toDto(owner);
     }
 
     public OwnerDTO findOne(Integer id) {
-        return null;
+        return ownerMapper.toDto(getOne(id));
     }
 
-    public void delete(Integer id) {}
+    public void delete(Integer id) {
+        Owner owner = getOne(id);
+        ownerRepository.delete(owner);
+    }
+
+    private Owner getOne(Integer id) {
+        return ownerRepository.findById(id).orElseThrow(() -> new BadRequestAlertException("Owner not found"));
+    }
 
 }
