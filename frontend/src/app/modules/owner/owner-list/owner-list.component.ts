4870c4cba9431db30568c4db191d6cf68278cce6
joao.calixto <joao.calixto@basis.com.br>
2021-07-03 20:16:56 -0300

    OWNER-LIST - listagem de owner finalizada

--- frontend/src/app/modules/owner/owner-list/owner-list.component.ts
+++ frontend/src/app/modules/owner/owner-list/owner-list.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {OwnerService} from "../../../shared/services/owner.service";
+import {OwnerUtil} from "../util/owner.util";
+import {OwnerListModel} from "../../../models/listing/owner-list.model";
+import {OwnerModel} from "../../../models/owner.model";
+import {PageNotificationService} from "@nuvem/primeng-components";
 
 @Component({
   selector: 'app-owner-list',
@@ -8,20 +12,36 @@ import {OwnerService} from "../../../shared/services/owner.service";
 })
 export class OwnerListComponent implements OnInit {
 
-  constructor(private ownerService: OwnerService) { }
+    constructor(
+        private ownerService: OwnerService,
+        private pageNotificationService: PageNotificationService
+        ) { }
 
-  ownerList = [];
-  // @BlockUI() blockUI: NgBlockUI;
+    ownerList = [];
+    columns = OwnerUtil.DEFAULT_COLUMNS;
 
-  ngOnInit(): void {
-      this.refreshTable();
-  }
+    ngOnInit(): void {
+        this.refreshTable();
+    }
 
     refreshTable() {
         this.ownerService.getAll().subscribe(res => {
             this.ownerList = res;
-            console.log(this.ownerList);
         })
+    }
+
+    getSelectedOnwer() {
+        const owner: OwnerModel = JSON.parse(localStorage.getItem("activeOwner"));
+        if(owner) {
+            return owner.id + " - " + owner.name;
+        }
+        return "";
+    }
+
+    setOwnerActive(owner: OwnerModel) {
+        this.pageNotificationService.addSuccessMessage("Owner selecionado com sucesso", "Sucesso!");
+        localStorage.setItem("activeOwner", JSON.stringify(owner));
+        this.getSelectedOnwer();
     }
 
 }
